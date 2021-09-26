2c81f2cb5b7b2d4b8aa03abbd753dd25f5520e37
Heder L Santos <73147296+MorcegoH@users.noreply.github.com>
2021-09-25 21:16:51 -0300

    Atualização
    Efetuado um novo treino em Java. Dessa vez, programa que traz a tabuada do número digitado.

---
+++ Tabuada.java
@@ -0,0 +1,33 @@
+
+package desafios;
+
+import java.util.Scanner;
+
+
+public class Tabuada { //Class sempre com letra maiuscula
+
+    public static void main(String[] args) {
+
+
+        //Estrutura Basica
+    	
+        System.out.println("##### TABUADA #####");
+        System.out.println("");
+        System.out.println("Digite o nº que deseja a tabuada: ");
+        Scanner in = new Scanner(System.in);
+        int fat1 = in.nextInt();
+        System.out.println("");
+
+        //Contador + calculo
+        
+        System.out.println("A tabuada do " + fat1 + " é:");
+        System.out.println("");
+        for (int fat2 = 0; fat2 < 11; fat2++) {
+            int produto = fat1 * fat2;
+            System.out.println(fat1 + " x " + fat2 + " = " + produto);
+            
+        }
+
+    }
+
+}
